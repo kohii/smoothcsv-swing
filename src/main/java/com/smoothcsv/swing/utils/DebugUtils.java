9818f4e4fd364cf5d59d5907fe3bdc654944535f
kohii <kohii.git@gmail.com>
2015-12-31 13:29:39 +0900

    Change debug log format

--- src/main/java/com/smoothcsv/swing/utils/DebugUtils.java
+++ src/main/java/com/smoothcsv/swing/utils/DebugUtils.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2015 kohii
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.smoothcsv.swing.utils;
 
@@ -63,11 +61,11 @@ public class DebugUtils {
   }
 
   public static void sysoutActionMap(JComponent com) {
-     final ActionMap am = com.getActionMap();
-     Object[] keys = am.allKeys();
-    
-     for (Object ks : keys) {
-     System.out.println(ks.toString() + " : " + am.get(ks));
-     }
+    final ActionMap am = com.getActionMap();
+    Object[] keys = am.allKeys();
+
+    for (Object ks : keys) {
+      System.out.println(ks.toString() + "\t" + am.get(ks));
+    }
   }
 }
